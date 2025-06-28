b8d2716af1e04559da3d429dd7b9f0547f8da390
MRamazan <mramazantrn28@gmail.com>
2025-06-28 05:39:18 +0300

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,7 @@
-from .ml_1m import ML1MDataset
 from .ml_20m import ML20MDataset
 from .AnimeRatings54m import AR54MDataset
 
 DATASETS = {
-    ML1MDataset.code(): ML1MDataset,
     ML20MDataset.code(): ML20MDataset,
     "AnimeRatings54M": AR54MDataset
 }
